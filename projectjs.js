30e574be6376c4b6bfd10700539d99dbdc58698b
satyam-bhaskar <satyambhaskar328@gmail.com>
2022-11-08 22:44:34 +0530

    commit

--- projectjs.js
+++ projectjs.js
@@ -1,26 +1,16 @@
 let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
-
 menuBtn.onclick = () =>{
    menuBtn.classList.toggle('fa-times');
    navbar.classList.toggle('active');
 };
-
-window.onscroll = () =>{
-   menuBtn.classList.remove('fa-times');
-   navbar.classList.remove('active');
-};
-
 var swiper = new Swiper(".home-slider", {
-   grabCursor:true,
    loop:true,
-   centeredSlides:true,
    navigation: {
      nextEl: ".swiper-button-next",
      prevEl: ".swiper-button-prev",
    },
 });
-
 const readMoreBtn = document.querySelector('.read-more-btn');
 const text = document.querySelector('.text');
 readMoreBtn.addEventListener('click',(e)=>{
@@ -31,66 +21,26 @@ readMoreBtn.addEventListener('click',(e)=>{
         readMoreBtn.innerText = 'Read More';
     }
 });
-
 var swiper = new Swiper(".food-slider", {
     grabCursor:true,
     loop:true,
     centeredSlides:true,
-    spaceBetween: 20,
     pagination: {
        el: ".swiper-pagination",
        clickable: true,
     },
     breakpoints: {
-       0: {
-         slidesPerView: 1,
-       },
-       700: {
-         slidesPerView: 2,
-       },
        1000: {
          slidesPerView: 3,
        },
     },
  });
-
-let previewContainer = document.querySelector('.food-preview-container');
-let previewBox = previewContainer.querySelectorAll('.food-preview');
-
-document.querySelectorAll('.food .slide').forEach(food =>{
-   food.onclick = () =>{
-      previewContainer.style.display = 'flex';
-      let name = food.getAttribute('data-name');
-      previewBox.forEach(preveiw =>{
-         let target = preveiw.getAttribute('data-target');
-         if(name == target){
-            preveiw.classList.add('active');
-         }
-      });
-   };
-});
-
-previewContainer.querySelector('#close-preview').onclick = () =>{
-   previewContainer.style.display = 'none';
-   previewBox.forEach(close =>{
-      close.classList.remove('active');
-   });
-};
-
 var swiper = new Swiper(".menu-slider", {
    grabCursor:true,
    loop:true,
-   autoHeight:true,
-   centeredSlides:true,
-   spaceBetween: 20,
    pagination: {
       el: ".swiper-pagination",
       clickable: true,
    },
 });
 
-
-let orderBtn = document.querySelector(".btn");
-orderBtn.addEventListener("click",function() {
-    console.log("hello")
-});
